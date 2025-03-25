263467d8e9a0a7b459684815adcf7ff8005fdda8
Lourencobrah <lourencospecter@gmail.com>
2025-03-25 16:58:16 -0300

    add auto port definition

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template, make_response
 import sqlite3
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -60,4 +61,5 @@ def view_logs():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000)) 
+    app.run(host='0.0.0.0', port=port)
